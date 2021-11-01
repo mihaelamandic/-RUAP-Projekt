8945381e2ffe0098412539161c9d91db88d9cc13
mihaelamandic <mihaela.mandic2829@gmail.com>
2021-11-01 22:58:49 +0100

    dodan index u poruci za ispis

--- Zadatak1/Zadatak1/Zadatak1.cpp
+++ Zadatak1/Zadatak1/Zadatak1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Mihaele Mandic\n";
+    std::cout << "Pozdrav iz aplikacije od Mihaele Mandic, 0165078948\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
